904590d19ea5cd7824edfa3ac82953a697d1507a
mikeyxx <79487718+mikeyxx@users.noreply.github.com>
2021-08-21 20:12:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Michael')
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
